fc2a7c5ba62e02aa1758e9e1abe4e51a85ed8b3b
joaovicttorsmelo <joaovicttorsilveiramelo@gmail.com>
2023-09-29 16:01:08 -0300

    Adicionando o Front-End do chat, comecando a criar as funcionanlidades do chat

--- codigo.py
+++ codigo.py
@@ -3,9 +3,40 @@ import flet as ft
 
 def main(pagina):
     texto = ft.Text("HashZap")
-    botao_iniciar = ft.ElevatedButton("iniciar chat")
+
+    nome_usuario = ft.TextField(label='Escreva seu nome')
+
+    campo_mensagem = ft.TextField(label='Escreva sua mensagem')
+    botao_enviar_mensagem = ft.ElevatedButton('Enviar Mansagem')
+
+    chat =ft.Column()
+    def entrar_popup(e):
+        pagina.add(chat)
+        popup.open = False
+        pagina.remove(texto)
+        pagina.remove(botao_iniciar)
+        pagina.add(ft.Row([campo_mensagem,botao_enviar_mensagem]))
+
+        pagina.update()
+
+
+    popup = ft.AlertDialog(open=False, modal=True, title=ft.Text("Bem vindo ao HashZap"),
+                           content=nome_usuario, actions=[ft.ElevatedButton('Entrar',on_click=entrar_popup)],)
+
+    def entrar_chat(e):
+        pagina.dialog = popup
+        popup.open = True
+        pagina.update()
+
+    botao_iniciar = ft.ElevatedButton("iniciar chat", on_click=entrar_chat)
     pagina.add(texto)
     pagina.add(botao_iniciar)
+
+
+
+
+
+
 
 
 ft.app(target=main, view=ft.WEB_BROWSER)
